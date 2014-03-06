7c04b9fa26717fec583a83121a6d11d6757136ad
antonio <antonio@antonio-N53SV>
2014-03-06 11:18:33 +0100

    Tirando una miaja. De camino a los descriptores

--- Primera/primera.cpp
+++ Primera/primera.cpp
@@ -7,120 +7,156 @@
 #include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/opencv.hpp>
 #include <vector>
-
+#include <sstream>
+#include <cmath>
 
 static const std::string OPENCV_WINDOW = "Image window";
 
 namespace enc = sensor_msgs::image_encodings;
 using namespace cv;
 
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+class PanoramCreator {
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+	// Propias
+	bool first;
+	Mat firstImg;
+	Mat secondImg;
 
 public:
-  ImageConverter() : it_(nh_)
-  {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
-      &ImageConverter::imageCbSift, this);
+	PanoramCreator() :
+			it_(nh_) {
+		this->first = true;
+		// Subscrive to input video feed and publish output video feed
+		image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
+				&PanoramCreator::imageCbSift, this);
 //    image_pub_ = it_.advertise("/image_converter/output_video", 1);
-    }
 
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
+	}
 
-  void imageCbSift(const sensor_msgs::ImageConstPtr& msg){
-	  cv_bridge::CvImagePtr cv_ptr;
-	  try
-	  {
-		  cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-	  }
-	  catch (cv_bridge::Exception& e)
-	  {
-		  ROS_ERROR("cv_bridge exception: %s", e.what());
-		  return;
-	  }
-	  vector<KeyPoint> keyPoints;
-	  Mat mask;
-	  Mat imageColor;
-	  Mat src_gray;
-	  cvtColor(src_gray, imageColor, CV_GRAY2BGR);
+	~PanoramCreator() {
+		cv::destroyWindow(OPENCV_WINDOW);
+	}
 
-	  cv::FeatureDetector* detector;
-	  detector = new SiftFeatureDetector(
-		 0, // nFeatures
-		 4, // nOctaveLayers
-		 0.04, // contrastThreshold
-		 10, //edgeThreshold
-		 1.6 //sigma
-		 );
+	void imageCbSift(const sensor_msgs::ImageConstPtr& msg) {
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+		} catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+		vector<KeyPoint> keyPoints;
 
-	  detector->detect(src_gray, keyPoints, mask);
+		Mat imageColor;
+		Mat src_gray;
 
-	  for (size_t i = 0; i < keyPoints.size(); i++) {
-		  circle(imageColor, keyPoints[i].pt, 3, CV_RGB(255, 0, 0));
-	  }
-	  Mat output;
-
-//	  drawKeypoints(img, keypoints, output, Scalar::all(-1));
-
-	  namedWindow("meh", CV_WINDOW_AUTOSIZE);
-	  imshow("meh", output);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-    {
-	  cv_bridge::CvImagePtr cv_ptr;
-	      try
-	      {
-	        cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-	      }
-	      catch (cv_bridge::Exception& e)
-	      {
-	        ROS_ERROR("cv_bridge exception: %s", e.what());
-	        return;
-	      }
-	      std::cerr<<" imagecb: "<<msg->header.frame_id<<" : "<<msg->header.seq<<" : "<<msg->header.stamp<<std::endl;
-
-	      cv::Mat src_gray;
-
-	   	  cvtColor( cv_ptr->image, src_gray, CV_BGR2GRAY );
-
-	   	  imshow("BN-Image", src_gray);
-	     Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create("FAST");
-	     std::vector<cv::KeyPoint> points;
-	     detector->detect(src_gray, points);
+		if (this->first) {
+			cvtColor(cv_ptr->image, this->firstImg, CV_BGR2GRAY);
+			this->first = false;
+			ROS_INFO("He obtenido la primera imagen y espero...");
+		} else {
+			cvtColor(cv_ptr->image, this->secondImg, CV_BGR2GRAY);
+			ROS_INFO("He obtenido nuevas y proceso...");
+			this->processPanoram();
+		}
 
 
-	      cv::Mat imageColor;
-	      cvtColor(src_gray, imageColor, CV_GRAY2BGR);
+	}
 
-	      for (size_t i = 0; i < points.size(); i++)
-	      {
-	        circle(imageColor, points[i].pt, 3, CV_RGB(255, 0, 0));
-	      }
-	      //se pueden pintar tambien con esta funcion
-	      //drawKeypoints(imageColor, points, imageColor, Scalar(255, 0, 0), DrawMatchesFlags::DRAW_OVER_OUTIMG);
+	void imageCb(const sensor_msgs::ImageConstPtr& msg) {
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+		} catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
+		std::cerr << " imagecb: " << msg->header.frame_id << " : "
+				<< msg->header.seq << " : " << msg->header.stamp << std::endl;
 
-	      imshow("Fast keypoints", imageColor);
+		cv::Mat src_gray;
 
+		cvtColor(cv_ptr->image, src_gray, CV_BGR2GRAY);
 
-	      cv::waitKey(3);
+		imshow("BN-Image", src_gray);
+		Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create("FAST");
+		std::vector<cv::KeyPoint> points;
+		detector->detect(src_gray, points);
 
-  }
+		cv::Mat imageColor;
+		cvtColor(src_gray, imageColor, CV_GRAY2BGR);
+
+		for (size_t i = 0; i < points.size(); i++) {
+			circle(imageColor, points[i].pt, 3, CV_RGB(255, 0, 0));
+		}
+		//se pueden pintar tambien con esta funcion
+		//drawKeypoints(imageColor, points, imageColor, Scalar(255, 0, 0), DrawMatchesFlags::DRAW_OVER_OUTIMG);
+
+		imshow("Fast keypoints", imageColor);
+
+		cv::waitKey(3);
+
+	}
+
+	void processPanoram() {
+		Mat mask;
+		vector<KeyPoint> keyPointsFirst;
+		vector<KeyPoint> keyPointsSecond;
+		Mat imageColorFirst;
+		Mat imageColorSecond;
+
+		cv::FeatureDetector* detector;
+		detector = new SiftFeatureDetector(
+				0, // nFeatures
+				4, // nOctaveLayers
+				0.04, // contrastThreshold
+				10, //edgeThreshold
+				1.6 //sigma
+				);
+
+		detector->detect(this->firstImg, keyPointsFirst, mask);
+		detector->detect(this->secondImg, keyPointsSecond, mask);
+
+		cvtColor(this->firstImg, imageColorFirst, CV_GRAY2BGR);
+		cvtColor(this->secondImg, imageColorSecond, CV_GRAY2BGR);
+
+		for (size_t i = 0; i < keyPointsFirst.size(); i++) {
+			circle(imageColorFirst, keyPointsFirst[i].pt, 3, CV_RGB(255, 0, 0));
+		}
+		for (size_t i = 0; i < keyPointsSecond.size(); i++) {
+			circle(imageColorSecond, keyPointsSecond[i].pt, 3, CV_RGB(255, 0, 0));
+		}
+
+		imshow("FirstImage", imageColorFirst);
+		imshow("SecondImage", imageColorSecond);
+
+		std::stringstream ss;
+		ss << "He encontrado "<<keyPointsFirst.size()<<" en la primera";
+		ROS_INFO(ss.str().c_str());
+		std::stringstream ss2;
+		ss2 << "He encontrado "<<keyPointsSecond.size()<<" en la segunda";
+		ROS_INFO(ss2.str().c_str());
+		cv::waitKey(3);
+
+		std::swap(firstImg, secondImg);
+	}
+
+	double euclideanDistance(KeyPoint kp1, KeyPoint kp2){
+		double dist;
+//		for(int i=0; i<kp1.size; i++){
+//			dist+=std::sqrt(std::pow(kp1.pt[i]-kp2.pt[i],2));
+//		}
+
+	}
 
 };
 
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+int main(int argc, char** argv) {
+	ros::init(argc, argv, "image_converter");
+	PanoramCreator ic;
+	ros::spin();
+	return 0;
 }
