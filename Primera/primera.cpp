26be1a0e61f070c08a2865f868a1dca9020ea516
antonio <antonio@antonio-N53SV>
2014-03-20 18:54:15 +0100

    To hecho

--- Primera/primera.cpp
+++ Primera/primera.cpp
@@ -26,13 +26,24 @@ class PanoramCreator {
 	Mat secondImg;
 
 public:
-	PanoramCreator() :
+	enum SOURCE {bag, camara};
+
+	PanoramCreator(SOURCE src) :
 			it_(nh_) {
 		this->first = true;
-		// Subscrive to input video feed and publish output video feed
-		image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
-				&PanoramCreator::imageCbSift, this);
-//    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+		switch(src){
+		case bag:
+			image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
+			&PanoramCreator::imageCbSift, this);
+			break;
+		case camara:
+			image_sub_ = it_.subscribe("/camera/image_raw", 1,
+			&PanoramCreator::imageCbSift, this);
+			break;
+		default:
+			image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
+			&PanoramCreator::imageCbSift, this);
+		}
 
 	}
 
@@ -130,8 +141,8 @@ public:
 			circle(imageColorSecond, keyPointsSecond[i].pt, 3, CV_RGB(255, 0, 0));
 		}
 
-		imshow("FirstImage", imageColorFirst);
-		imshow("SecondImage", imageColorSecond);
+//		imshow("FirstImage", imageColorFirst);
+//		imshow("SecondImage", imageColorSecond);
 
 		SiftDescriptorExtractor extractor;
 
@@ -142,69 +153,123 @@ public:
 
 		//-- Step 3: Matching descriptor vectors using FLANN matcher
 		FlannBasedMatcher matcher;
-		std::vector< DMatch > matches;
-		matcher.match( desciptorsImg1, desciptorsImg2, matches );
+		std::vector< std::vector<DMatch> > matches;
+//		matcher.match( desciptorsImg1, desciptorsImg2, matches ); // Esto lo haríamos para tal. Pero queremos...
+		matcher.knnMatch(desciptorsImg1, desciptorsImg2, matches, 2); // Encontramos 2 más cercanas
 
+		/** Esto es basurilla **/
 		double max_dist = 0; double min_dist = 100, second_min = 100;
 
 		//-- Quick calculation of max and min distances between keypoints
 		for( int i = 0; i < desciptorsImg1.rows; i++ ){
-			double dist = matches[i].distance;
-			if( dist < second_min ){
-				if(dist < min_dist)	min_dist = dist;
-				else second_min = dist;
-			}
+			double dist = matches[i][0].distance;
+				if(dist < min_dist)
+					min_dist = dist;
 			if( dist > max_dist ) max_dist = dist;
 		}
-
-		printf("-- Max dist : %f \n", max_dist );
-		printf("-- Second min dist : %f \n", second_min );
+//
+//		printf("-- Max dist : %f \n", max_dist );
+//		printf("-- Second min dist : %f \n", second_min );
 		printf("-- Min dist : %f \n", min_dist );
-		printf("-- El mejor por 0.8 es: %f\n", second_min*0.8);
+//		printf("-- El mejor por 0.8 es: %f\n", second_min*0.8);
 
 		  //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-//		std::vector<DMatch> good_matches;
-//
-//		for (int i = 0; i < desciptorsImg1.rows; i++) {
-//			if (matches[i].distance < 3 * min_dist) {
-//				good_matches.push_back(matches[i]);
-//			}
-//		}
-
-		float umbral = 3*min_dist;
-		if(min_dist < umbral && min_dist < 0.8*second_min){
-			printf("Por lo tanto se dibuja\n", second_min*0.8);
-			Mat img_matches;
-			drawMatches(this->firstImg, keyPointsFirst, this->secondImg, keyPointsSecond,
-					matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-					vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
-
-			std::stringstream ss;
-			ss << "He encontrado "<<matches.size()<<" en la primera";
-			ROS_INFO(ss.str().c_str());
-	//		std::stringstream ss2;
-	//		ss2 << "He encontrado "<<desciptorsImg2.size()<<" en la segunda";
-	//		ROS_INFO(ss2.str().c_str());
-
-			//-- Show detected matches
-			imshow("Good Matches", img_matches);
-			std::vector<Point2f> kImg1;
-			std::vector<Point2f> kImg2;
-
-			for( int i = 0; i < matches.size(); i++ )			{
-			    //-- Get the keypoints from the good matches
-			    kImg1.push_back( keyPointsFirst[ matches[i].queryIdx ].pt );
-			    kImg2.push_back( keyPointsSecond[ matches[i].trainIdx ].pt );
-			}
-			Mat H = findHomography( kImg1, kImg2, CV_RANSAC );
-			Mat output;
-
-//			perspectiveTransform( this->secondImg, output, H);
-			hconcat(this->firstImg, this->secondImg, output);
-			imshow("Result", output);
+		std::vector<DMatch> good_matches;
+		for(int i=0; i<desciptorsImg1.rows; i++){ // Esto lo hacemos por separado para poder analizar cuantos quitamos con cada uno
+			if(matches[i][0].distance < 0.8*matches[i][1].distance && matches[i][0].distance < 3*min_dist)
+				good_matches.push_back(matches[i][0]);
 		}
 
+		Mat img_matches;
+		drawMatches(this->firstImg, keyPointsFirst, this->secondImg, keyPointsSecond,
+				good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+		std::stringstream ss;
+		ss << "He encontrado "<<good_matches.size()<<" matches decentes";
+		ROS_INFO(ss.str().c_str());
+
+		//-- Show detected matches
+//		imshow("Good Matches", img_matches);
+		std::vector<Point2f> kImg1;
+		std::vector<Point2f> kImg2;
+
+		for( int i = 0; i < good_matches.size(); i++ )			{
+			//-- Get the keypoints from the good matches
+			kImg1.push_back( keyPointsFirst[ good_matches[i].queryIdx ].pt );
+			kImg2.push_back( keyPointsSecond[ good_matches[i].trainIdx ].pt );
+		}
+
+		Mat H = findHomography( kImg1, kImg2, CV_RANSAC );
+
+		const std::vector<Point2f> points_ant_transformed(keyPointsFirst.size());
+		std::vector<Point2f> keypoints_ant_vector(keyPointsFirst.size());
+		cv::KeyPoint::convert(keyPointsFirst,keypoints_ant_vector);
+
+		//transformamos los puntos de la imagen anterior
+		perspectiveTransform( keypoints_ant_vector, points_ant_transformed, H);
+
+		//creamos una copia de la imagen actual que usaremos para dibujar
+		Mat transformed_image;
+		cvtColor(this->secondImg, transformed_image, CV_GRAY2BGR);
+
+		//los que esten mas lejos que este parametro se consideran outliers (o que la transformacion está mal calculada)
+		//este valor es orientativo, podeis cambiarlo y ajustarlo a los valores
+		float distance_threshold=10.0;
+		int contdrawbuenos=0;
+		int contdrawmalos=0;
+		for ( int i =0;i<good_matches.size();i++)
+		{
+		    int ind        = good_matches.at(i).trainIdx ;
+		    int ind_Ant    = good_matches.at(i).queryIdx;
+
+		    cv::Point2f p=        keyPointsSecond.at(ind).pt;
+		    cv::Point2f p_ant=    points_ant_transformed[ind_Ant];
+
+		    circle( transformed_image, p_ant, 5, Scalar(255,0,0), 2, 8, 0 ); //ant blue
+		    circle( transformed_image, p, 5, Scalar(0,255,255), 2, 8, 0 ); //current yellow
+
+		    Point pointdiff = p - points_ant_transformed[ind_Ant];
+		        float distance_of_points=cv::sqrt(pointdiff.x*pointdiff.x + pointdiff.y*pointdiff.y);
+
+		    if(distance_of_points < distance_threshold){ // los good matches se pintan con un circulo verde mas grand
+		        contdrawbuenos++;
+		        circle( transformed_image, p, 9, Scalar(0,255,0), 2, 8, 0 ); //current red
+		    }
+		    else{
+		        contdrawmalos++;
+		        line(transformed_image,p,p_ant,Scalar(0, 0, 255),1,CV_AA);
+		    }
+		}
+
+//		imshow( "transformed", transformed_image );
+//		imwrite("/home/antonio/ejemplowrite.png",transformed_image );
+
+		cv::Mat result;
+
+		warpPerspective(this->firstImg, result, H,
+				cv::Size(1000, this->firstImg.rows));
+		cv::Mat half(result, cv::Rect(0, 0, this->secondImg.cols, this->secondImg.rows));
+		this->secondImg.copyTo(half);
+		result.copyTo(this->secondImg);
+		imshow("Easy Merge Result", this->secondImg);
+//		Mat H = findHomography( kImg1, kImg2, CV_RANSAC );
+//		Mat output;
+//
+//		perspectiveTransform( this->secondImg, output, H);
+//
+//		// Para montar las imagenes
+//		double alpha = 0.5; double beta;
+//		Mat dst;
+//
+//		beta = ( 1.0 - alpha );
+//		addWeighted( this->firstImg, alpha, output, beta, 0.0, dst);
+//
+////		hconcat(this->firstImg, this->secondImg, output);
+//		imshow("Result", dst);
+
 		cv::waitKey(3);
+		imwrite("/home/antonio/result.png",result);
 		std::swap(firstImg, secondImg);
 	}
 
@@ -212,7 +277,7 @@ public:
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "image_converter");
-	PanoramCreator ic;
+	PanoramCreator ic(PanoramCreator::camara);
 	ros::spin();
 	return 0;
 }
