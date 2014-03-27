99630db92861e328d47c01f725e185498016e32c
Leopoldo Pla <leopoldoplasempere@gmail.com>
2014-03-27 19:33:34 +0100

    Dale

--- Primera/primera.cpp
+++ Primera/primera.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <sstream>
 #include <cmath>
+#include <exception>
 
 static const std::string OPENCV_WINDOW = "Image window";
 
@@ -16,68 +17,40 @@ namespace enc = sensor_msgs::image_encodings;
 using namespace cv;
 
 class PanoramCreator {
-	ros::NodeHandle nh_;
-	image_transport::ImageTransport it_;
-	image_transport::Subscriber image_sub_;
-	image_transport::Publisher image_pub_;
-	// Propias
-	bool first;
-	Mat firstImg;
-	Mat secondImg;
-
 public:
 	enum SOURCE {bag, camera, kinect};
+	enum DETECTOR {SIFT_D, SURF_D, FAST, MSER, ORB_D};
+	enum EXTRACTOR {SIFT_E, SURF_E, BRIEF, ORB_E};
 
-	PanoramCreator(SOURCE src) : it_(nh_) {
+	PanoramCreator(SOURCE src, DETECTOR dtc, EXTRACTOR ext, float filterValue1, float filterValue2 ) : it_(nh_) {
 		this->first = true;
+		this->filter1 = filterValue1;
+		this->filter2 = filterValue2;
+		this->detector = dtc;
+		this->extractor = ext;
+
 		switch(src){
 		case bag:
 			image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
-			&PanoramCreator::imageCbSift, this);
+			&PanoramCreator::imageCb, this);
 			break;
 		case camera:
 			image_sub_ = it_.subscribe("/camera/image_raw", 1,
-			&PanoramCreator::imageCbSift, this);
+			&PanoramCreator::imageCb, this);
 			break;
 		case kinect:
 			image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
-			&PanoramCreator::imageCbSift, this);
-			break
+			&PanoramCreator::imageCb, this);
+			break;
 		default:
 			image_sub_ = it_.subscribe("/camera/rgb/image_color", 1,
-			&PanoramCreator::imageCbSift, this);
+			&PanoramCreator::imageCb, this);
 		}
 
 	}
 
 	~PanoramCreator() {
 		cv::destroyWindow(OPENCV_WINDOW);
-	}
-
-	void imageCbSift(const sensor_msgs::ImageConstPtr& msg) {
-		cv_bridge::CvImagePtr cv_ptr;
-		try {
-			cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-		} catch (cv_bridge::Exception& e) {
-			ROS_ERROR("cv_bridge exception: %s", e.what());
-			return;
-		}
-		vector<KeyPoint> keyPoints;
-
-		Mat imageColor;
-		Mat src_gray;
-
-		if (this->first) {
-			cvtColor(cv_ptr->image, this->firstImg, CV_BGR2GRAY);
-			this->first = false;
-			ROS_INFO("He obtenido la primera imagen y espero...");
-		} else {
-			cvtColor(cv_ptr->image, this->secondImg, CV_BGR2GRAY);
-			ROS_INFO("He obtenido nuevas y proceso...");
-			this->processPanoram();
-		}
-
-
 	}
 
 	void imageCb(const sensor_msgs::ImageConstPtr& msg) {
@@ -88,32 +61,27 @@ public:
 			ROS_ERROR("cv_bridge exception: %s", e.what());
 			return;
 		}
-		std::cerr << " imagecb: " << msg->header.frame_id << " : "
-				<< msg->header.seq << " : " << msg->header.stamp << std::endl;
 
-		cv::Mat src_gray;
+		vector<KeyPoint> keyPoints;
 
-		cvtColor(cv_ptr->image, src_gray, CV_BGR2GRAY);
+		Mat imageColor;
+		Mat src_gray;
 
-		imshow("BN-Image", src_gray);
-		Ptr<cv::FeatureDetector> detector = cv::FeatureDetector::create("FAST");
-		std::vector<cv::KeyPoint> points;
-		detector->detect(src_gray, points);
-
-		cv::Mat imageColor;
-		cvtColor(src_gray, imageColor, CV_GRAY2BGR);
-
-		for (size_t i = 0; i < points.size(); i++) {
-			circle(imageColor, points[i].pt, 3, CV_RGB(255, 0, 0));
+		if (this->first) {
+			cvtColor(cv_ptr->image, this->firstImg, CV_BGR2GRAY);
+			this->first = false;
+			this->firstImgColor = cv_ptr->image;
+			ROS_INFO("He obtenido la primera imagen y espero...");
+		} else {
+			cvtColor(cv_ptr->image, this->secondImg, CV_BGR2GRAY);
+			this->secondImgColor = cv_ptr->image;
+			ROS_INFO("He obtenido nuevas y proceso...");
+			this->processPanoram();
 		}
-		//se pueden pintar tambien con esta funcion
-		//drawKeypoints(imageColor, points, imageColor, Scalar(255, 0, 0), DrawMatchesFlags::DRAW_OVER_OUTIMG);
 
-		imshow("Fast keypoints", imageColor);
-
-		cv::waitKey(3);
 
 	}
+
 
 	void processPanoram() {
 		Mat mask;
@@ -123,19 +91,30 @@ public:
 		Mat imageColorSecond;
 
 		cv::FeatureDetector* detector;
-		detector = new SiftFeatureDetector(
-				0, // nFeatures
-				4, // nOctaveLayers
-				0.04, // contrastThreshold
-				10, //edgeThreshold
-				1.6 //sigma
-			);
+
+		switch(this->detector){
+		case SIFT_D:
+			detector = new SiftFeatureDetector();
+			break;
+		case SURF_D:
+			detector = new SurfFeatureDetector();
+			break;
+		case FAST:
+			detector = new FastFeatureDetector();
+			break;
+		case MSER:
+			detector = new MserFeatureDetector();
+			break;
+		case ORB_D:
+			detector = new OrbFeatureDetector();
+			break;
+		default:
+			detector = new OrbFeatureDetector();
+		}
 
 		detector->detect(this->firstImg, keyPointsFirst, mask);
 		detector->detect(this->secondImg, keyPointsSecond, mask);
 
-		cvtColor(this->firstImg, imageColorFirst, CV_GRAY2BGR);
-		cvtColor(this->secondImg, imageColorSecond, CV_GRAY2BGR);
 
 		for (size_t i = 0; i < keyPointsFirst.size(); i++) {
 			circle(imageColorFirst, keyPointsFirst[i].pt, 3, CV_RGB(255, 0, 0));
@@ -147,12 +126,29 @@ public:
 //		imshow("FirstImage", imageColorFirst);
 //		imshow("SecondImage", imageColorSecond);
 
-		SiftDescriptorExtractor extractor;
+		cv::DescriptorExtractor* extractor;
+
+		switch(this->extractor){
+		case SIFT_E:
+			extractor = new SiftDescriptorExtractor();
+			break;
+		case SURF_E:
+			extractor = new SurfDescriptorExtractor();
+			break;
+		case BRIEF:
+			extractor = new BriefDescriptorExtractor();
+			break;
+		case ORB_E:
+			extractor = new OrbDescriptorExtractor();
+			break;
+		default:
+			extractor = new OrbDescriptorExtractor();
+		}
 
 		Mat desciptorsImg1, desciptorsImg2;
 
-		extractor.compute(this->firstImg, keyPointsFirst, desciptorsImg1);
-		extractor.compute(this->secondImg, keyPointsSecond, desciptorsImg2);
+		extractor->compute(this->firstImg, keyPointsFirst, desciptorsImg1);
+		extractor->compute(this->secondImg, keyPointsSecond, desciptorsImg2);
 
 		//-- Step 3: Matching descriptor vectors using FLANN matcher
 		FlannBasedMatcher matcher;
@@ -179,7 +175,7 @@ public:
 		  //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
 		std::vector<DMatch> good_matches;
 		for(int i=0; i<desciptorsImg1.rows; i++){ // Esto lo hacemos por separado para poder analizar cuantos quitamos con cada uno
-			if(matches[i][0].distance < 0.8*matches[i][1].distance && matches[i][0].distance < 3*min_dist)
+			if(matches[i][0].distance < this->filter1*matches[i][1].distance && matches[i][0].distance < this->filter2*min_dist)
 				good_matches.push_back(matches[i][0]);
 		}
 
@@ -203,84 +199,91 @@ public:
 			kImg2.push_back( keyPointsSecond[ good_matches[i].trainIdx ].pt );
 		}
 
-		Mat H = findHomography( kImg1, kImg2, CV_RANSAC );
+		if(good_matches.size() >= 4){
+			Mat H = findHomography( kImg1, kImg2, CV_RANSAC );
+			const std::vector<Point2f> points_ant_transformed(keyPointsFirst.size());
+			std::vector<Point2f> keypoints_ant_vector(keyPointsFirst.size());
+			cv::KeyPoint::convert(keyPointsFirst,keypoints_ant_vector);
 
-		const std::vector<Point2f> points_ant_transformed(keyPointsFirst.size());
-		std::vector<Point2f> keypoints_ant_vector(keyPointsFirst.size());
-		cv::KeyPoint::convert(keyPointsFirst,keypoints_ant_vector);
+			//transformamos los puntos de la imagen anterior
+			perspectiveTransform( keypoints_ant_vector, points_ant_transformed, H);
 
-		//transformamos los puntos de la imagen anterior
-		perspectiveTransform( keypoints_ant_vector, points_ant_transformed, H);
+			//creamos una copia de la imagen actual que usaremos para dibujar
+			Mat transformed_image;
+			cvtColor(this->secondImg, transformed_image, CV_GRAY2BGR);
 
-		//creamos una copia de la imagen actual que usaremos para dibujar
-		Mat transformed_image;
-		cvtColor(this->secondImg, transformed_image, CV_GRAY2BGR);
+			//los que esten mas lejos que este parametro se consideran outliers (o que la transformacion está mal calculada)
+			//este valor es orientativo, podeis cambiarlo y ajustarlo a los valores
+			float distance_threshold=10.0;
+			int contdrawbuenos=0;
+			int contdrawmalos=0;
+			for ( int i =0;i<good_matches.size();i++)
+			{
+				int ind        = good_matches.at(i).trainIdx ;
+				int ind_Ant    = good_matches.at(i).queryIdx;
 
-		//los que esten mas lejos que este parametro se consideran outliers (o que la transformacion está mal calculada)
-		//este valor es orientativo, podeis cambiarlo y ajustarlo a los valores
-		float distance_threshold=10.0;
-		int contdrawbuenos=0;
-		int contdrawmalos=0;
-		for ( int i =0;i<good_matches.size();i++)
-		{
-		    int ind        = good_matches.at(i).trainIdx ;
-		    int ind_Ant    = good_matches.at(i).queryIdx;
+				cv::Point2f p=        keyPointsSecond.at(ind).pt;
+				cv::Point2f p_ant=    points_ant_transformed[ind_Ant];
 
-		    cv::Point2f p=        keyPointsSecond.at(ind).pt;
-		    cv::Point2f p_ant=    points_ant_transformed[ind_Ant];
+				circle( transformed_image, p_ant, 5, Scalar(255,0,0), 2, 8, 0 ); //ant blue
+				circle( transformed_image, p, 5, Scalar(0,255,255), 2, 8, 0 ); //current yellow
 
-		    circle( transformed_image, p_ant, 5, Scalar(255,0,0), 2, 8, 0 ); //ant blue
-		    circle( transformed_image, p, 5, Scalar(0,255,255), 2, 8, 0 ); //current yellow
+				Point pointdiff = p - points_ant_transformed[ind_Ant];
+					float distance_of_points=cv::sqrt(pointdiff.x*pointdiff.x + pointdiff.y*pointdiff.y);
 
-		    Point pointdiff = p - points_ant_transformed[ind_Ant];
-		        float distance_of_points=cv::sqrt(pointdiff.x*pointdiff.x + pointdiff.y*pointdiff.y);
+				if(distance_of_points < distance_threshold){ // los good matches se pintan con un circulo verde mas grand
+					contdrawbuenos++;
+					circle( transformed_image, p, 9, Scalar(0,255,0), 2, 8, 0 ); //current red
+				}
+				else{
+					contdrawmalos++;
+					line(transformed_image,p,p_ant,Scalar(0, 0, 255),1,CV_AA);
+				}
+			}
 
-		    if(distance_of_points < distance_threshold){ // los good matches se pintan con un circulo verde mas grand
-		        contdrawbuenos++;
-		        circle( transformed_image, p, 9, Scalar(0,255,0), 2, 8, 0 ); //current red
-		    }
-		    else{
-		        contdrawmalos++;
-		        line(transformed_image,p,p_ant,Scalar(0, 0, 255),1,CV_AA);
-		    }
+			imshow( "transformed", transformed_image );
+//		imwrite("~/ejemplowrite.png",transformed_image );
+
+			cv::Mat result;
+
+			warpPerspective(firstImgColor, result, H,
+					cv::Size(2000, this->firstImg.rows));
+			cv::Mat half(result, cv::Rect(0, 0, this->secondImg.cols, this->secondImg.rows));
+			secondImgColor.copyTo(half);
+			result.copyTo(secondImgColor);
+
+			imshow("Easy Merge Result", result);
+
+			cv::waitKey(3);
+//			imwrite("~/result.png",result);
+			std::swap(firstImgColor, secondImgColor);
+			std::swap(firstImg, secondImg);
 		}
-
-		imshow( "transformed", transformed_image );
-//		imwrite("/home/antonio/ejemplowrite.png",transformed_image );
-
-		cv::Mat result;
-
-		warpPerspective(this->firstImg, result, H,
-				cv::Size(2000, this->firstImg.rows));
-		cv::Mat half(result, cv::Rect(0, 0, this->secondImg.cols, this->secondImg.rows));
-		this->secondImg.copyTo(half);
-		result.copyTo(this->secondImg);
-		imshow("Easy Merge Result", this->secondImg);
-//		Mat H = findHomography( kImg1, kImg2, CV_RANSAC );
-//		Mat output;
-//
-//		perspectiveTransform( this->secondImg, output, H);
-//
-//		// Para montar las imagenes
-//		double alpha = 0.5; double beta;
-//		Mat dst;
-//
-//		beta = ( 1.0 - alpha );
-//		addWeighted( this->firstImg, alpha, output, beta, 0.0, dst);
-//
-////		hconcat(this->firstImg, this->secondImg, output);
-//		imshow("Result", dst);
-
-		cv::waitKey(3);
-		imwrite("/home/antonio/result.png",result);
-		std::swap(firstImg, secondImg);
 	}
+
+private:
+	ros::NodeHandle nh_;
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber image_sub_;
+	image_transport::Publisher image_pub_;
+	// Propias
+	float filter1;
+	float filter2;
+	DETECTOR detector;
+	EXTRACTOR extractor;
+	bool first;
+	Mat firstImg;
+	Mat firstImgColor;
+	Mat secondImg;
+	Mat secondImgColor;
+
+
 
 };
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "image_converter");
-	PanoramCreator ic(PanoramCreator::bag);
+	PanoramCreator ic(PanoramCreator::bag, PanoramCreator::FAST, PanoramCreator::SIFT_E, 0.8, 2);
 	ros::spin();
 	return 0;
 }
